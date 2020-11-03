86d57bac6ff878a061290891e3b9a6db71f28bdf
Mickaël Schoentgen <contact@tiger-222.fr>
2020-11-04 00:19:24 +0100

    Purgation

--- scripts/stubs.py
+++
@@ -1,8 +0,0 @@
-"""Type annotations."""
-from collections import namedtuple
-from typing import Dict, Tuple, Union
-
-Definitions = Union[str, Tuple[str, ...]]
-Parts = Tuple[str, ...]
-Word = namedtuple("Word", "pronunciation, genre, etymology, definitions")
-Words = Dict[str, Word]
